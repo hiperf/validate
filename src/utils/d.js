a0f6c6274c256554df61d938450959fbe729bd1e
Pavel Sokolov <tirius.dev@gmail.com>
2024-07-07 22:10:17 +0300

    refactor: refactor dictionary

--- src/utils/d.js
+++ src/utils/d.js
@@ -1,11 +1,11 @@
 import langs from '../lang/index';
 
-export default function (key, options = {}, lang = "en") {
+export default function(key, options = {}, lang = 'en') {
 	if (!(lang in langs))
-		throw new Error(`lang "${lang}" doesn't exist object langs`);
+		throw new Error(`Lang "${lang}" doesn't exist object langs`);
 
 	if (!(key in langs[lang]))
-		throw new Error(`dictionary key "${key}" does not exist`);
+		throw new Error(`Dictionary key "${key}" does not exist`);
 
 	let result = langs[lang][key];
 
