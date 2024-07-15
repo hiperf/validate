c3b27689a34eeaac1af29da8f1104eed38bc060c
hiperf <hiperf.dev@gmail.com>
2024-07-15 15:12:11 +0300

    test(isArray): added more detailed tests

--- tests/isArray.test.js
+++ tests/isArray.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('isArray schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			names: {
+				isArray: true
+			}
+		};
+		const data = {
+			names: ['John', 'Mia', 'Felix']
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('isArray schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			names: {
+				isArray: true
+			}
+		};
+		const data = {
+			names: {name: 'John'}
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('isArray validator test', () => {
 	function f({ isArray }) {
