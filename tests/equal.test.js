cb48c852d13f3969340487914f37bf4ff1a32fec
hiperf <hiperf.dev@gmail.com>
2024-07-15 15:11:59 +0300

    test(equal): added more detailed tests

--- tests/equal.test.js
+++ tests/equal.test.js
@@ -1,6 +1,37 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('equal schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			letter: { equal: 'a' }
+		};
+		const data = { letter: 'a' };
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('equal schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			letter: { equal: 'a' }
+		};
+		const data = { letter: 'b' };
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('equal validator test', () => {
 	function f({ equal }) {
