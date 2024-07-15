01cc7445bc37be366dc9812dcc6175e3c5dc3b4e
hiperf <hiperf.dev@gmail.com>
2024-07-15 15:12:35 +0300

    test(isEmail): added more detailed tests

--- tests/isEmail.test.js
+++ tests/isEmail.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('isEmail schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			userEmail: {
+				isEmail: true
+			}
+		};
+		const data = {
+			userEmail: 'user@example.com'
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('isEmail schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			userEmail: {
+				isEmail: true
+			}
+		};
+		const data = {
+			userEmail: 'us@r@example.com'
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('isEmail validator test', () => {
 	function f({ isEmail }) {
