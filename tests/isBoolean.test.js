d214bd886f683bbeb1d421709ab65fa270bd2f68
hiperf <hiperf.dev@gmail.com>
2024-07-15 15:12:25 +0300

    test(isBoolean): added more detailed tests

--- tests/isBoolean.test.js
+++ tests/isBoolean.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('isBoolean schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			state: {
+				isBoolean: true
+			}
+		};
+		const data = {
+			state: true
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('isBoolean schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			state: {
+				isBoolean: true
+			}
+		};
+		const data = {
+			state: 0
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('isBoolean validator test', () => {
 	function f({ isBoolean }) {
