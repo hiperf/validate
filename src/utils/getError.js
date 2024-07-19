dc36167be8fed2951085e0e37a9bfa8612cc2b92
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:46:03 +0300

    refactor: simplified getError util

--- src/utils/getError.js
+++ src/utils/getError.js
@@ -2,12 +2,15 @@ import d from './d';
 import isObject from '../validators/isObject';
 
 /** 
- * @typedef {object} GetErrorParams
+ * @typedef {Object} GetErrorParams
  * @property {*} validatorConfigValue - validator value
  * @property {*} validatorConfig - validator config
  * @property {string} validatorName - validator name
  * @property {string} fieldName - field to validate
  * @property {*} dataValue - validator input value
+ * @property {string} lang - errors language
+ * @property {Object.<string, Object>} userLocales - user defined locales
+ * @property {Object.<string, Object>} libLocales - lib locales
  */
 
 /**
@@ -18,12 +21,8 @@ import isObject from '../validators/isObject';
 export default function({ validatorConfigValue, validatorConfig, validatorName, fieldName, dataValue, lang, userLocales, libLocales }) {
 	let error = '';
 
-	if (isObject(validatorConfig)) {
-		if (!('value' in validatorConfig)) {
-			throw new Error('Missing "value" key in validator config');
-		}
-
-		if ('error' in validatorConfig) error = validatorConfig.error;
+	if (isObject(validatorConfig) && ('error' in validatorConfig)) {
+		error = validatorConfig.error;
 	}
 
 	return error ? error : `${fieldName}: ` + d(validatorName, { e: validatorConfigValue, v: dataValue }, lang, userLocales, libLocales);
