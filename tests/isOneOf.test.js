36bde9c631f07c56f47a2ad863c62a87a77968ab
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:26:59 +0300

    test(isOneOf): added more detailed test

--- tests/isOneOf.test.js
+++ tests/isOneOf.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('isOneOf schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			language: {
+				isOneOf: ['C++', 'Python', 'JS']
+			}
+		};
+		const data = {
+			language: 'C++'
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('isOneOf schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			language: {
+				isOneOf: ['C++', 'Python', 'JS']
+			}
+		};
+		const data = {
+			language: 'C'
+		};
+		
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('isOneOf validator test', () => {
 	function f({ isOneOf }) {
