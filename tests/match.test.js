f0525375176f52c52ee04971d6e8fe76f93e8335
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:43:30 +0300

    test(match): added more detailed test

--- tests/match.test.js
+++ tests/match.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('match schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			message: {
+				match: /🐱/
+			}
+		};
+		const data = {
+			message: '🐱'
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('match schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			message: {
+				match: /🐱/
+			}
+		};
+		const data = {
+			message: 'cat'
+		};
+		
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('match validator test', () => {
 	function f({ match }) {
