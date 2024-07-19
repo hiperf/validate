33761ab8526b4bf9a2a1f335c4c3fbec22276b6f
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:28:12 +0300

    test(isString): added more detailed test

--- tests/isString.test.js
+++ tests/isString.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('isString schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			message: {
+				isString: true
+			}
+		};
+		const data = {
+			message: 'Hello world'
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('isString schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			message: {
+				isString: true
+			}
+		};
+		const data = {
+			message: true
+		};
+		
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('isString validator test', () => {
 	function f({ isString }) {
