07d1e39a8a9928715e2befe271766936abf32e30
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:48:11 +0300

    test(max): added more detailed test

--- tests/max.test.js
+++ tests/max.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('max schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			count: {
+				max: 5
+			}
+		};
+		const data = {
+			count: 1
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('max schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			count: {
+				max: 5
+			}
+		};
+		const data = {
+			count: 6
+		};
+		
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('max validator test', () => {
 	function f({ max }) {
