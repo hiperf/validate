24bb45c26969b7c56fcf78bef2ba2df2de182c6e
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:24:59 +0300

    test(isObject): added more detailed test

--- tests/isObject.test.js
+++ tests/isObject.test.js
@@ -1,6 +1,49 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('isObject schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			user: {
+				isObject: true
+			}
+		};
+		const data = {
+			user: {
+				name: 'John'
+			}
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('isObject schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			user: {
+				isObject: true
+			}
+		};
+		const data = {
+			user: ['John']
+		};
+		
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('isObject validator test', () => {
 	function f({ isObject }) {
