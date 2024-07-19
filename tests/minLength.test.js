214fa891222f7dd09f256917056b0a999d6c91a8
hiperf <hiperf.dev@gmail.com>
2024-07-19 13:51:48 +0300

    test(minLength): added more detailed test

--- tests/minLength.test.js
+++ tests/minLength.test.js
@@ -1,6 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, assert } from 'vitest';
 import * as cjs from './dist/cjs/index';
 import * as es from './dist/es/index';
+
+test('minLength schema test', () => {
+	function f({ validate }) {
+		const schema = {
+			message: {
+				minLength: 5
+			}
+		};
+		const data = {
+			message: 'hello'
+		};
+
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(true);
+		assert.isEmpty(errors, 'errors should be empty');
+	}
+	f(cjs);
+	f(es);
+});
+
+test('minLength schema negative test', () => {
+	function f({ validate }) {
+		const schema = {
+			message: {
+				minLength: 5
+			}
+		};
+		const data = {
+			message: 'hell'
+		};
+		
+		const { isValid, errors } = validate(schema, data);
+
+		expect(isValid).toBe(false);
+		assert.isNotEmpty(errors, 'errors should not be empty');
+		expect(errors[0], 'error message should contain field name').toContain(Object.keys(data)[0]);
+	}
+	f(cjs);
+	f(es);
+});
 
 test('minLength validator test', () => {
 	function f({ minLength }) {
